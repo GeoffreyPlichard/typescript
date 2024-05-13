a2d84b15e5021028810e5cbee4c9c933f7e1c7c8
Geoffrey <geoffreyplichard@gmail.com>
2024-05-13 17:10:20 +0200

    add enums

--- Exemples/stats/build/index.js
+++ Exemples/stats/build/index.js
@@ -1,2 +1,31 @@
 "use strict";
-console.log('ok');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs")); // Need to install Node type definition file (see package.json)
+const matches = fs_1.default
+    .readFileSync('football.csv', {
+    encoding: 'utf-8' // Need to provide encoding type or we get a buffer
+})
+    .split('\n')
+    .map((row) => {
+    return row.split(',');
+});
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+;
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
--- Exemples/stats/src/index.ts
+++ Exemples/stats/src/index.ts
@@ -1,1 +1,30 @@
-console.log('ok')
+
+import fs from 'fs'; // Need to install Node type definition file (see package.json)
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8' // Need to provide encoding type or we get a buffer
+  })
+  .split('\n')
+  .map((row: string): string[] => {
+    return row.split(',');
+  });
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+};
+
+let manUnitedWins = 0;
+
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
