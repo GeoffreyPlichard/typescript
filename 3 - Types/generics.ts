990956f1b82d480f1734f1adc3d84627ff18011f
Geoffrey <geoffreyplichard@gmail.com>
2024-05-14 10:32:36 +0200

    improve generics example

--- 3 - Types/generics.ts
+++ 3 - Types/generics.ts
@@ -23,3 +23,31 @@ class ArrayOfAnything<T> {
 }
 
 new ArrayOfAnything<string>(['a', 'b', 'c']);
+
+// Constraints
+
+class Car {
+  print() {
+    console.log('Iam a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('Iam a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+// printHousesOrCars([1, 2, 3, 4]); // numbers don't satisfy Printable interface
+printHousesOrCars<House>([new House(), new House()]);
+printHousesOrCars<Car>([new Car(), new Car()]);
