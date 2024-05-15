61f6fc4b101b44f684e781daf877e6010c037b3d
Geoffrey <geoffreyplichard@gmail.com>
2024-05-15 19:55:22 +0200

    add express with typescript

---
+++ Exemples/express-with-typescript/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieSession({keys: ['sefsc']})); // Key is used to encrypt the session
+app.use(router);
+
+app.listen(3001, () => {
+  console.log('Listening on port 3001');
+});
