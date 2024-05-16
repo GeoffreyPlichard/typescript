21be9546af0b4d08a04cfb18081e98819faaa326
Geoffrey <geoffreyplichard@gmail.com>
2024-05-16 15:04:50 +0200

    add guard for decorators methods

--- Exemples/express-with-typescript/src/controllers/decorators/routes.ts
+++ Exemples/express-with-typescript/src/controllers/decorators/routes.ts
@@ -1,11 +1,18 @@
 import 'reflect-metadata';
 import { Methods } from './Methods';
 import { MetadataKeys } from './MetadataKeys';
+import { RequestHandler } from 'express';
+
+// A guard to check if the decorators methods takes a request and response as argument
+// and return nothing
+interface RouteHandlerDescriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 // Create factory decorators
 function routeBinder(method: string) {
   return function(path: string) {
-    return function(target: any, key: string, desc: PropertyDescriptor) {
+    return function(target: any, key: string, desc: RouteHandlerDescriptor) {
       Reflect.defineMetadata(MetadataKeys.path, path, target, key);
       Reflect.defineMetadata(MetadataKeys.method, method, target, key);
     }
