b2119ce4dff5ac7949a22ab7c86b805ed070f2dc
Geoffrey <geoffreyplichard@gmail.com>
2024-05-23 09:59:48 +0200

    add more example isp

---
+++ 6 - Design Patterns/SOLID/Isp2.ts
@@ -0,0 +1,47 @@
+// ISP: Post Creation Example
+
+interface Post {
+  title: string;
+  content: string;
+}
+
+interface Comment {
+  title: string;
+  content: string;
+}
+
+interface PostCreator {
+  createPost(post: Post): void;
+}
+
+interface CommentCreator {
+  createComment(comment: Comment): void;
+}
+
+interface PostSharer {
+  sharePosts(post: Post): void;
+}
+
+class Admin implements PostCreator, CommentCreator, PostSharer {
+  createPost(post: Post): void {
+    console.log('Admin is creating a post');
+  }
+
+  createComment(comment: Comment): void {
+    console.log('Admin is creating a comment');
+  }
+
+  sharePosts(post: Post): void {
+    console.log('Admin is sharing a post');
+  }
+}
+
+class RegularUser implements CommentCreator, PostSharer {
+  createComment(comment: Comment): void {
+    console.log('Admin is creating a comment');
+  }
+
+  sharePosts(post: Post): void {
+    console.log('Admin is sharing a post');
+  }
+}
